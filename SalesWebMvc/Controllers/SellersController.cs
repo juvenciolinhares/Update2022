c2abd72ac6b541d9dee9ed99fa4ec10f9911c60b
Juvencio Linhares <jlinhares@cpqi.com>
2022-07-28 17:12:15 -0300

    SellerService and basic FindAll

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,10 +9,29 @@ namespace SalesWebMvc.Controllers
 {
     public class SellersController : Controller
     {
-        //por padrao o gerador scaffold criou uma pagina index
-        public IActionResult Index()
+
+        //declarar uma dependencia de sellerservice:
+        private readonly SellerService _sellerService;
+
+
+        //cont p injetar dependencia:
+        public SellersController(SellerService sellerService)
         {
-            return View();
+            _sellerService = sellerService;
+        }
+        
+        public IActionResult Index()//controlador
+        {
+            //implementar a chama de sellerService.FindAll que retorna uma lista de seller
+            var list = _sellerService.FindAll();//model
+
+            //lista vai ser passada como arg p ser gerado uma lista 
+            return View(list);//view
         }
     }
 }
+/*mvc acontecendo nas linhas:
+ *  public IActionResult Index()//controlador
+ *  var list = _sellerService.FindAll();//model
+ *  return View(list);//view
+ */
---
+++ SalesWebMvc/Services/SellerService.cs
@@ -0,0 +1,31 @@
+﻿using SalesWebMvc.Data;
+using SalesWebMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Services
+{
+    public class SellerService
+    {
+        private readonly SalesWebMvcContext _context;  //previnir que a dependencia n pode ser alterada: readonly
+
+        
+        //construtor pra ocorrer a injeção de dependencia:
+
+        public SellerService(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+
+        /*operação FindAll p retornar uma lista com tds os vendedores do bd:
+         * operação sícrona: vai rodar o acesso ao bd=> _context.Seller.ToList()
+         * e a app vai ficar bloqueada esperando a operação terminar.
+         */
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();//acessa a fonte de dados de vendedores e converte p lista
+        }
+    }
+}
