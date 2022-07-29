cf117e20fbeb28588dbaa57d5b786a4e30ca0909
Juvencio Linhares <jlinhares@cpqi.com>
2022-07-29 20:27:07 -0300

    Implementing simple search

---
+++ SalesWebMvc/Services/SalesRecordService.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SalesWebMvc.Data;
+using SalesWebMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Services
+{
+    public class SalesRecordService
+    {
+        private readonly SalesWebMvcContext _context;  //previnir que a dependencia n pode ser alterada: readonly
+
+
+       
+
+        public SalesRecordService(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+
+       
+        //operação assincrona que busca os registros de venda por data com as datas minima e maxima opcionais:
+        public async Task<List<SalesRecord>> FindByDateAsync(DateTime? minDate, DateTime? maxDate)//? significa opcional
+        {
+            //encontrar as vendas nesse intervalo de datas:
+            var result = from obj in _context.SalesRecord select obj;//constroi o obj iqueryable
+
+            if (minDate.HasValue)//se tem uma data mínima
+            {
+                result = result.Where(x => x.Date >= minDate.Value);// maior ou igual a data mínima
+            }
+            if (maxDate.HasValue)// se tem data máxima
+            {
+                result = result.Where(x => x.Date <= maxDate.Value);//menor ou igual a data maxima
+            }
+
+            return await result
+                .Include(x => x.Seller)//inclui vendedor
+                .Include(x => x.Seller.Department)//inclui departamento
+                .OrderByDescending(x => x.Date)//inclui data em ordem decresente
+                .ToListAsync();
+        }
+    }
+}
+
+
